6e123d3d9e60c7b38301e16a0f977e1e5a31b0c1
Eduardo Teixeira <eduardo.apteixeira00@gmail.com>
2024-06-30 22:15:27 -0300

    feat: frontend initial implementation

---
+++ frontend/src/plugins/vuetify.ts
@@ -0,0 +1,16 @@
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+
+export default createVuetify({
+  theme: {
+    defaultTheme: "light",
+  },
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
